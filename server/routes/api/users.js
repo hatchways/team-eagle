a7f919187dc339f4821cd4bc279bbabab09e3d4f
Hakeem Almidan <hakeemmidan@gmail.com>
2020-05-18 15:44:14 -0700

    Create '/login' route

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -12,7 +12,7 @@ const validateLoginInput = require("../../validation/login");
 const User = require("../../models/User");
 
 
-// @route POST api/users/register
+// @route POST /register
 // @desc Register user
 // @access Public
 router.post("/register", (req, res) => {
@@ -47,3 +47,59 @@ router.post("/register", (req, res) => {
     }
   });
 });
+
+// @route POST /login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+  // Form validation
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(404).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(404)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
+
+module.exports = router;
