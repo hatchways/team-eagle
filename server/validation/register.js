9352f30e9d0e55f81b712ccd35943d0cb0e685b6
Hakeem Almidan <hakeemmidan@gmail.com>
2020-05-18 15:31:59 -0700

    Create '/register' route

--- server/validation/register.js
+++ server/validation/register.js
@@ -25,7 +25,7 @@ module.exports = function validateRegisterInput(data) {
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+  if (!Validator.isLength(data.password, { min: 6 })) {
     errors.password = "Password must be at least 6 characters";
   }
 
