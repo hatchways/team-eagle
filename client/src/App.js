ac994919ba930c28eae60c81ee13fa0391158956
thiagonemecek <tgnemecek@gmail.com>
2020-05-19 16:30:31 -0400

    Add redirection to Signup route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 
@@ -12,16 +12,21 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route
-          exact
-          path="/signup"
-          render={() => <LandingPage form="signup" />}
-        />
-        <Route
-          exact
-          path="/login"
-          render={() => <LandingPage form="login" />}
-        />
+        <Switch>
+          <Route
+            exact
+            path="/signup"
+            render={() => <LandingPage form="signup" />}
+          />
+          <Route
+            exact
+            path="/login"
+            render={() => <LandingPage form="login" />}
+          />
+          <Route path="*">
+            <Redirect to="/signup" />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
   );
