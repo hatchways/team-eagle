48060885219fed76ddbe249c4417ce1f97b4b5d1
reydon227 <dhruvarora@delhinerds.com>
2020-05-21 12:01:19 -0400

    removed console log from delete.poll

--- server/routes/polls/delete.route.js
+++ server/routes/polls/delete.route.js
@@ -4,8 +4,7 @@ const Poll = require("../../models/polls");
 
 // Route to delete a poll
 router.post("/", (req, res) => {
-    const pollId = req.body.id;        
-    console.log(pollId)
+    const pollId = req.body.id;            
 	Poll.findByIdAndDelete(pollId, (error) => {
 		if (error) return res.status(400).send(error);
 		else {            
