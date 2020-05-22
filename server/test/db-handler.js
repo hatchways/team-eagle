a7b55ea7a6eb05a9a7cd7ca63acfc2e7eb2fd6da
Hakeem Almidan <hakeemmidan@gmail.com>
2020-05-21 20:12:23 -0700

    Add 'db-handler.js' file to handle creating a mock db for testing

---
+++ server/test/db-handler.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const mongod = new MongoMemoryServer();
+
+// @fileDesc : this file is used to create a mock database for testing
+  // the db is cleared and removed after testing
+
+/**
+ * Connect to the in-memory database.
+ */
+module.exports.connect = async () => {
+  const uri = await mongod.getConnectionString();
+
+  const mongooseOpts = {
+    useNewUrlParser: true,
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+  };
+
+  await mongoose.connect(uri, mongooseOpts);
+};
+
+/**
+ * Drop database, close the connection and stop mongod.
+ */
+module.exports.closeDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongod.stop();
+};
+
+/**
+ * Remove all the data for all db collections.
+ */
+module.exports.clearDatabase = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany();
+  }
+};
+
+// reference:
+// https://dev.to/paulasantamaria/testing-node-js-mongoose-with-an-in-memory-database-32np
