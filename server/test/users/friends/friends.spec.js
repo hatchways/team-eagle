8a992f4cab4355d63d4f6b76aa534c4a38546b3c
Hakeem Almidan <hakeemmidan@gmail.com>
2020-05-25 18:32:55 -0700

    write tests for '.../friends/suggestions' route

--- server/test/users/friends/friends.spec.js
+++ server/test/users/friends/friends.spec.js
@@ -78,35 +78,80 @@ describe('GET /users/:userId/friends', () => {
 
 /******************************************************************************/
 
-// describe('GET /users/:userId/friends/suggestions', () => {
-//   context('When request author is logged in', () => {
-//     before(() => {
-//       // do api call here;
-//     });
+describe('GET /users/:userId/friends/suggestions', () => {
+  before((done) => {
+    let user = {
+      email: 'seedEmail1@gmail.com',
+      password: '12345678',
+    };
 
-//     it('it returns 200 status code', () => {
-//       this.response.should.have.status(200);
-//     });
+    chai
+      .request(app)
+      .post('/auth/login')
+      .send(user)
+      .end((err, res) => {
+        this.loggedInUser = res.body;
+        this.cookie = res.headers['set-cookie'].find((el) =>
+          el.includes('jwt=')
+        );
+        done();
+      });
+  });
 
-//     it('it returns a list of friends in an array', () => {
-//       // expect response to have a list of friends in an array
-//     });
-//   });
+  context('When request author is logged in', () => {
+    before((done) => {
+      chai
+        .request(app)
+        .get(`/users/${this.loggedInUser._id}/friends/suggestions/name=user 2`)
+        .set('Cookie', this.cookie)
+        .end((err, res) => {
+          this.response = res;
+          done();
+        });
+    });
 
-//   context('When request author is not logged in', () => {
-//     before(() => {
-//       // do api call here;
-//     });
+    it('it returns 200 status code', () => {
+      this.response.should.have.status(200);
+    });
 
-//     it('it returns 401 status code', () => {
-//       this.response.should.have.status(401);
-//     });
-//   });
-// });
+    it('it returns a list of friends in an array', () => {
+      expect(this.response.body.friends).to.be.an('array');
+    });
+
+    it('it filters friend suggestions based on name param', () => {
+      expect(this.response.body.friends).to.deep.include({
+        name: 'seed user 2',
+      });
+    });
+  });
+
+  context('When request author is not logged in', () => {
+    before((done) => {
+      chai
+        .request(app)
+        .delete('/auth/logout')
+        .set('Cookie', this.cookie)
+        .end(() => {});
+
+      chai
+        .request(app)
+        .get(`/users/${this.loggedInUser._id}/friends/suggestions`)
+        .set('Cookie', this.cookie)
+        .end((err, res) => {
+          this.response = res;
+          done();
+        });
+    });
+
+    it('it returns 401 status code', () => {
+      this.response.should.have.status(401);
+    });
+  });
+});
 
 // /******************************************************************************/
 
-// describe('POST /users/:userId/friends/follow', () => {
+// describe('PUT /users/:userId/friends/follow', () => {
 //   context('When request author is logged in', () => {
 //     before(() => {
 //       // do api call here;
