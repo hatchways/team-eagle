f016b0ee5a16c5f512deb22e0fe7cc4dbd1c91d6
dotslash227 <dhruv@dhruvarora.in>
2020-05-28 17:52:38 -0400

    sockets basics

--- server/app.js
+++ server/app.js
@@ -55,6 +55,8 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+// Socket.io works
+
 // Error handler
 app.use(function (err, req, res, next) {
   // Set locals, only providing error in development
--- server/models/user.js
+++ server/models/user.js
@@ -19,6 +19,10 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  active: {
+    type: Boolean,
+    required: false,
+  },
   date: {
     type: Date,
     default: Date.now,
