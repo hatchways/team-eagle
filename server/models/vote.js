d30877034e1bfb41e666d02479b4ef4b82dec6c6
Hakeem Almidan <hakeemmidan@gmail.com>
2020-06-02 13:17:27 -0700

    Add requirement constraints to userId and pollId in Vote model

--- server/models/vote.js
+++ server/models/vote.js
@@ -4,10 +4,12 @@ const Schema = mongoose.Schema;
 const VoteSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
+    required: true,
     ref: 'users',
   },
   pollId: {
     type: Schema.Types.ObjectId,
+    required: true,
     ref: 'polls',
   },
 });
