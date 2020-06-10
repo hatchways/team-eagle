6dacba050a5df96147721ac894c172abf71ac78e
Hakeem Almidan <hakeemmidan@gmail.com>
2020-06-10 16:24:50 -0700

    Remove un-used import

--- server/routes/polls/polls.route.js
+++ server/routes/polls/polls.route.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const passport = require('passport');
-const mongoose = require('mongoose');
 const router = express.Router();
 
 const Poll = require('../../models/poll');
